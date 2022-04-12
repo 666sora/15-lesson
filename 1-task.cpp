04f7ccfeb39ee8ed835925fc684579c242878276
666sora <pavelbys03@gmail.com>
2022-04-12 21:13:46 +0700

    1-task done without testing

--- 1-task.cpp
+++ 1-task.cpp
@@ -2,7 +2,30 @@
 #include <vector>
 
 int main() {
-    
+    int n;
+    std::cout << "Input vector size: ";
+    std::cin >> n;
+    std::vector<int> vec(n);
+    for (int i = 0; i < vec.size(); i++) {
+        std::cout << "Input number: ";
+        std::cin >> vec[i];
+    }
+    int sum = 0;
+    int findI, findJ;
+    for (int i = vec.size() - 1; i >= 0; i--) {
+        for (int j = 0; j <= i; j++) {
+            int tempSum = 0;
+            for (int k = j; k <= i; k++) {
+                tempSum += vec[k];
+            }
+            if (tempSum > sum) {
+                sum = tempSum;
+                findI = j;
+                findJ = i;
+            }
+        }            
+    }
+    std::cout << "i: " << findI << std::endl << "j: " << findJ;
 }
 
 /*
