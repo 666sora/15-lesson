d9d8763a25ee2a60dae25d9a8d2072b0c8edf7bc
666sora <pavelbys03@gmail.com>
2022-04-12 21:29:32 +0700

    2-task done without testing

--- 2-task.cpp
+++ 2-task.cpp
@@ -2,7 +2,27 @@
 #include <vector>
 
 int main() {
-    
+    int n;
+    std::cout << "Input vector size: ";
+    std::cin >> n;
+    std::vector<int> vec(n);
+    for (int i = 0; i < vec.size(); i++) {
+        std::cout << "Input number: ";
+        std::cin >> vec[i];
+    }
+    int result;
+    std::cout << "Input result number: ";
+    std::cin >> result;
+    int findI, findJ;
+    for (int i = 0; i < vec.size(); i++) {
+        for (int j = 0; j < vec.size(); j++) {
+            if (vec[i] + vec[j] == result && i != j) {
+                findJ = vec[i];
+                findI = vec[j];
+            }
+        }
+    }
+    std::cout << findI << " + " << findJ << " = " << result;
 }
 /*
 Вам даётся массив целых чисел и одно число -- результат. 
