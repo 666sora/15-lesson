bc248fc446c3b0e5083884478af3e48276f41092
666sora <pavelbys03@gmail.com>
2022-04-15 05:59:27 +0700

    4-task done with o(n^2) time

--- 4-task.cpp
+++ 4-task.cpp
@@ -2,10 +2,27 @@
 #include <vector>
 
 int main() {
-
+    int n;
+    std::cout << "Input vector size: ";
+    std::cin >> n;
+    std::vector<int> vec(n);
+    for (int i = 0; i < vec.size(); i++) {
+        std::cout << "Input number: ";
+        std::cin >> vec[i];
+    }
+    for (int i = 0; i < vec.size(); i++) {
+        for (int j = 0; j < vec.size() - i - 1; j++) {
+            if (abs(vec[j]) > abs(vec[j + 1])) {
+                std::swap(vec[j], vec[j + 1]);
+            }
+        }
+    }
+    for (int i = 0; i < vec.size(); i++) {
+        std::cout << vec[i] << " ";
+    }
 }
 
 /*
-Вам даётся массив целых чисел, отсортированных по возрастанию. 
+Вам даётся массив целых чисел, отсортированных по возрастанию.
 Необходимо вывести его на экран отсортированным в порядке возрастания модуля чисел
 */
