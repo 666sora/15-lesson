570e6d7b9d2588045dae85ca65f590ad5027af9c
666sora <pavelbys03@gmail.com>
2022-04-17 05:34:42 +0700

    new algorithm

--- 3-task.cpp
+++ 3-task.cpp
@@ -4,27 +4,36 @@
 int main() {
     std::vector<int> vec;
     int n = 0;
+    bool sort = false;
     while (n != -2) {
         do {
             std::cout << "Input number: ";
             std::cin >> n;
-            if (n != -1 && n != -2) vec.push_back(n);
-        } while (n != -1 && n != -2);
-        if (n == -1) {
-            bool find = false;
-            for (int i = 0; i < vec.size() && !find; i++) {
-                for (int j = 0; j < vec.size() - i - 1; j++) {
-                    if (vec[j] > vec[j + 1]) {
-                        int temp = vec[j];
-                        vec[j] = vec[j + 1];
-                        vec[j + 1] = temp;
+            if (vec.size() == 5 && !sort) {
+                for (int i = 0; i < vec.size(); i++) {
+                    for (int j = 0; j < vec.size() - i - 1; j++) {
+                        if (vec[j] > vec[j + 1]) {
+                            std::swap(vec[j], vec[j + 1]);
+                        }
                     }
                 }
-                if (i == 5) {
-                    std::cout << "Fifth number is " << vec[i - 1] << std::endl;
-                    find = true;
-                }
+                sort = true;
             }
+            if (n != -1 && n != -2) {
+                if (vec.size() >= 5 && vec[4] > n) {
+                    vec.push_back(n);
+                    for (int i = vec.size() - 1; i > 0; i--) {
+                        if (vec[i] < vec[i - 1]) {
+                            std::swap(vec[i], vec[i - 1]);
+                        }
+                    }
+                    vec.pop_back();
+                }
+                else if (vec.size() < 5) vec.push_back(n);
+            }
+        } while (n != -1 && n != -2);
+        if (n == -1) {
+            std::cout << "Fifth number is " << vec[4] << std::endl;
         }
     }
 }
